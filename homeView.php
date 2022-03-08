c893c18d618804da9095f49bb1232a47f8eea304
K1908379 <K1908379@kingston.ac.uk>
2022-03-08 13:19:28 +0000

    Create homeView.php

---
+++ homeView.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content ="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <title></title>
+    </head>
+    <body>
+        <div class="container-fluid" style="border-bottom:5px solid #000;">
+            <h1>Medixcode</h1>
+        </div>
+        <div class="container-field">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>User Login</h2>
+                    <form>
+                        <label for="username">User Name:</label><br>
+                        <input type="text" id="uName" name="uName"><br>
+                        <label for="password">Password:</label><br>
+                        <input type="text" id="pWord" name="pWord"><br>
+                        <input type="submit" value="Submit">
+                    </form>
+                </div>
+                <div class="col-sm-6">
+                    <h2>Staff Login</h2>
+                    <form>
+                        <label for="username">User Name:</label><br>
+                        <input type="text" id="uName" name="uName"><br>
+                        <label for="password">Password:</label><br>
+                        <input type="text" id="pWord" name="pWord"><br>
+                        <input type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
